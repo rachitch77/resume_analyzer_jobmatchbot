cd263896712ef9d7c6794be823b80b0f19503cfe
Rachit <rachit.ch77@gmail.com>
2025-07-18 17:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 # ------------------- CONFIG -------------------
-SHEET_ID = st.secrets["gcp"]["sheet_id"]  # Spreadsheet ID from secrets
+SHEET_ID = st.secrets["gcp_service_account"]["sheet_id"]
+  # Spreadsheet ID from secrets
 TAB_NAME = "Users"
 DEBUG_MODE = False
 
