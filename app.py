72a8fac042cda3a715a21fb1bfac885f7cca1db2
Rachit <rachit.ch77@gmail.com>
2026-02-11 07:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 TAB_NAME = "Users"
 DEBUG_MODE = False
 client = OpenAI(
-    api_key=st.secrets["aiintellisense"]["sk_ai_0f46cd905b24297fe67fea06bbb82da23e58b9a22269f6c6"],
+    api_key=st.secrets["aiintellisense"]["api_key"],
     base_url="https://api.aiintellisense.com/v1"
 )
 
@@ -213,6 +213,7 @@ Job Description:
 # ------------------- RUN APP -------------------
 if __name__ == "__main__":
     main()
+
 
 
 
